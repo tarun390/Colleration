03f07651c7f21dda498388b5fa555f2f02137131
tarun390 <73883651+tarun390@users.noreply.github.com>
2021-06-25 20:10:31 +0530

    Update colleration.py

--- colleration.py
+++ colleration.py
@@ -1,24 +1,23 @@
 import csv
 import numpy as np
 
-
 def getDataSource(data_path):
-    marks_in_percentage = []
-    days_present = []
+    marks_pg = []
+    days_pg = []
     with open(data_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            marks_in_percentage.append(float(row["Marks In Percentage"]))
-            days_present.append(float(row["Days Present"]))
+            marks_pg.append(float(row["Marks In Percentage"]))
+            days_pg.append(float(row["Days Present"]))
 
-    return {"x" : marks_in_percentage, "y": days_present}
+    return {"x" : marks_pg, "y": days_pg}
 
 def findCorrelation(datasource):
     correlation = np.corrcoef(datasource["x"], datasource["y"])
     print("Correlation between Marks in percentage and Days present :-  \n--->",correlation[0,1])
 
 def setup():
-    data_path  = "./data/Student Marks vs Days Present.csv"
+    data_path  = ""# <--mam pls put the data link
 
     datasource = getDataSource(data_path)
     findCorrelation(datasource)
